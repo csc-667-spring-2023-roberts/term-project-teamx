9b73fe1581fc2da2cfd3e7c09b04de042e396b49
pie240 <andyalmeida191@gmail.com>
2023-04-14 12:49:43 -0700

    Added Middleware (Not used), Serving static files from backend functionality

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const path = require("path");
 const createError = require("http-errors");
+
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "backend", "static")));
 
 const rootRoutes = require("./backend/routes/root");
 
