89fb78bf1637e7aa13188159fa63923ba7d9cc07
pie240 <andyalmeida191@gmail.com>
2023-04-14 13:26:42 -0700

    Added Content Creation utilizing Pug +html +css

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const createError = require("http-errors");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
+app.set("views", path.join(__dirname, "backend", "views"));
+app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "backend", "static")));
+
+const PORT = process.env.PORT || 3000;
 
 const rootRoutes = require("./backend/routes/root");
 
