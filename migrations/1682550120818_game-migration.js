f8a72bdbba703c647c27f8cb7198c104ddcc36d7
pie240 <andyalmeida191@gmail.com>
2023-05-11 17:45:26 -0700

    Fix Migration for Table, Add Column: is_started

--- migrations/1682550120818_game-migration.js
+++ migrations/1682550120818_game-migration.js
@@ -4,11 +4,6 @@
 exports.up = pgm => {
   pgm.createTable("game", {
     id: "id",
-    is_started: {
-      type: "boolean",
-      notNull: true,
-      default: false,
-    },
     created_at: {
       type: "timestamp",
       notNull: true,
--- migrations/1682650432802_fix-game-again.js
+++ migrations/1682650432802_fix-game-again.js
@@ -6,6 +6,15 @@ exports.shorthands = undefined;
  * @param {import("node-pg-migrate/dist/types").MigrationBuilder} pgm
  */
 exports.up = (pgm) => {
+    pgm.addColumn("game", {
+        is_started: {
+            type: "boolean",
+            notNull: true,
+            default: false,
+        }
+    });
 };
 
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+    pgm.dropColumn("game", "is_started");
+};
