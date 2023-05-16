bf8ed3b3f477fe4d64842b1123758d02d75c642a
pie240 <andyalmeida191@gmail.com>
2023-05-15 22:43:57 -0700

    Repaired Lobby -> unneccesary data and html

--- backend/routes/static/lobby.js
+++ backend/routes/static/lobby.js
@@ -6,20 +6,15 @@ const router = express.Router();
 router.get("/", async (request, response) => {
   const { id: user_id } = request.session.user;
 
-  const games = await Games.getAvailableGames(user_id);
-  const joinedgames = await Games.getGames(user_id);
   const availableGames = await Games.getAvailableGames(user_id); //Games that the user is not in that is_running = false
   const reJoinGames = await Games.getGames(user_id); //Games that the user is in that is_running = true
   const runningGames = await Games.getRunningGames(user_id);
-
-  console.log(games);
 
   response.render("lobby", {
     title: "Team X term project",
     availableGames: availableGames,
     reJoinGames: reJoinGames,
     runningGames: runningGames,
-    joinedgames : joinedgames,
   });
 });
 
